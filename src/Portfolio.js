3707817f688f8666bcb77fe887b9037b489769d8
chris-butterworth <chris_butterworth@icloud.com>
2023-09-18 20:09:49 +0100

    created portfolio post

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -3,13 +3,7 @@ export const portfolio = {
 	created_at: Date.now(),
 	author: 'chris-butterworth',
 	title: 'Pinned: A portfolio piece by Chris Butterworth',
-	body: `This full stack forum app was created whilst on the Software Development course at <a href="">Northcoders</a> to demonstrate my abilities etc. The back end API was created using Express.js and PostgreSQL, the front end is made with React. The picture above is of 
-	
-	
-	Big up yourself
-	
-	
-	No one is going to be hyped about you if you arent hyped about yourself`,
+	body: `This full stack forum app was created whilst on the Software Development course at Northcoders to demonstrate my abilities etc. The back end API was created using Express.js and PostgreSQL, the front end is made with React.`,
 	votes: 9000,
 	article_img_url: '/src/assets/milburn-koko.jpeg',
 	comment_count: 0,
