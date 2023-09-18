8a85a6e83a311393328f6689b5c8267e35bb0720
chris-butterworth <chris_butterworth@icloud.com>
2023-09-18 18:33:13 +0100

    created pinned post with portfolio credit content

---
+++ src/Portfolio.js
@@ -0,0 +1,17 @@
+export const portfolio = {
+	article_id: 0,
+	created_at: Date.now(),
+	author: 'chris-butterworth',
+	title: 'Pinned: A portfolio piece by Chris Butterworth',
+	body: `This full stack forum app was created whilst on the Software Development course at <a href="">Northcoders</a> to demonstrate my abilities etc. The back end API was created using Express.js and PostgreSQL, the front end is made with React. The picture above is of 
+	
+	
+	Big up yourself
+	
+	
+	No one is going to be hyped about you if you arent hyped about yourself`,
+	votes: 9000,
+	article_img_url: '/src/assets/milburn-koko.jpeg',
+	comment_count: 0,
+	topic: 'coding',
+}
--- src/components/Contents.jsx
+++ src/components/Contents.jsx
@@ -1,64 +1,64 @@
-import { Route, Routes, Navigate } from "react-router-dom";
-import { useState } from "react";
-import Articles from "./Articles/Articles";
-import ArticlePage from "./ArticlePage/ArticleSingle";
-import Comments from "./Comments/Comments";
-import UserProfile from "./User/UserProfile";
-import UserArticles from "./User/UserArticles";
+import { Route, Routes, Navigate } from 'react-router-dom'
+import { useState } from 'react'
+import Articles from './Articles/Articles'
+import ArticlePage from './ArticlePage/ArticleSingle'
+import Comments from './Comments/Comments'
+import UserProfile from './User/UserProfile'
+import UserArticles from './User/UserArticles'
+import CreditPage from './ArticlePage/CreditPage'
 
 export default function Contents() {
-  const [articles, setArticles] = useState([]);
-  const [article, setArticle] = useState([]);
-  const [comments, setComments] = useState([]);
-  // the searchParam states must live here because the Articles page gets reloaded when selecting a new topic, which resets all the queries
-  const [sortBy, setSortBy] = useState("created_at");
-  const [order, setOrder] = useState("DESC");
-  const [user, setUser] = useState("jessjelly");
+	const [articles, setArticles] = useState([])
+	const [article, setArticle] = useState([])
+	const [comments, setComments] = useState([])
+	// the searchParam states must live here because the Articles page gets reloaded when selecting a new topic, which resets all the queries
+	const [sortBy, setSortBy] = useState('created_at')
+	const [order, setOrder] = useState('DESC')
+	const [user, setUser] = useState('jessjelly')
 
-  return (
-    <main>
-      <Routes>
-        <Route
-          path="/topic/:topic"
-          element={
-            <Articles
-              articles={articles}
-              setArticles={setArticles}
-              sortBy={sortBy}
-              order={order}
-              setOrder={setOrder}
-              setSortBy={setSortBy}
-            />
-          }
-        />
-        <Route
-          path="/article/:article_id"
-          element={<ArticlePage article={article} setArticle={setArticle} />}
-        >
-          <Route
-            path=""
-            element={
-              <Comments
-                comments={comments}
-                setComments={setComments}
-                user={user}
-              />
-            }
-          />
-        </Route>
-        <Route path="/user/:username" element={<UserProfile />}>
-          <Route
-            path=""
-            element={
-              <UserArticles
-                articles={articles}
-                setArticles={setArticles}
-              />
-            }
-          />
-        </Route>
-        <Route path="*" element={<Navigate to="/topic/all" />} />
-      </Routes>
-    </main>
-  );
+	return (
+		<main>
+			<Routes>
+				<Route path="/credit" element={<CreditPage />} />
+
+				<Route
+					path="/topic/:topic"
+					element={
+						<Articles
+							articles={articles}
+							setArticles={setArticles}
+							sortBy={sortBy}
+							order={order}
+							setOrder={setOrder}
+							setSortBy={setSortBy}
+						/>
+					}
+				/>
+				<Route
+					path="/article/:article_id"
+					element={<ArticlePage article={article} setArticle={setArticle} />}
+				>
+					<Route
+						path=""
+						element={
+							<Comments
+								comments={comments}
+								setComments={setComments}
+								user={user}
+							/>
+						}
+					/>
+				</Route>
+				<Route path="/user/:username" element={<UserProfile />}>
+					<Route
+						path=""
+						element={
+							<UserArticles articles={articles} setArticles={setArticles} />
+						}
+					/>
+				</Route>
+				<Route path="*" element={<Navigate to="/topic/all" />} />
+			</Routes>
+		</main>
+	)
 }
