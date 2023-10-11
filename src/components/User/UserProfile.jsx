6700c8d404d576a8a594ffef00bbdc97120ae5d1
chris-butterworth <chris_butterworth@icloud.com>
2023-10-11 21:24:25 +0100

    styled user profile

--- src/components/User/UserProfile.jsx
+++ src/components/User/UserProfile.jsx
@@ -1,35 +1,18 @@
 import { useContext } from "react";
 import { Context } from "../contexts/Contexts";
 import UserArticles from "./UserArticles";
+import { Card, CardContent, Typography } from "@mui/material";
 
 export default function UserProfile({ articles, setArticles }) {
   const { user } = useContext(Context);
   return (
     <div>
-      <div className="profile-header">
-        <h3>Logged in user: {user.username}</h3>
-        <br />
-        <h4>This is a portfolio piece created by Chris Butterworth</h4>
-        <p>
-          The back end API was created using Express.js and PostgreSQL, the
-          front end is made with React.
-          <br /> It remains a work in progress, I will continue to update and
-          add new features. <br />
-        </p>
-        <br />
-        <a href="www.linkedin.com/in/chris-butterworth-74b77a25a">
-          Linked In
-        </a>{" "}
-        <span> / </span>
-        <a href="https://github.com/chris-butterworth/cb-news">
-          Back end code{" "}
-        </a>{" "}
-        <span> / </span>
-        <a href="https://github.com/chris-butterworth/fe-cb-news">
-          Front end code{" "}
-        </a>
-        <br />
-      </div>
+      <Card>
+        <CardContent>
+          <Typography variant="h4">Logged in user: {user.username}</Typography>
+          <Typography>Login and user auth features coming soon</Typography>
+        </CardContent>
+      </Card>
       <UserArticles articles={articles} setArticles={setArticles} />
     </div>
   );
